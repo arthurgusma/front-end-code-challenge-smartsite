cc96d8227b345f81620645c22fba81af76242252
Arthur Gusmao <arthur.cabj@hotmail.com>
2024-05-01 12:37:56 -0300

    feat: api call to get locations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
-export default function Home() {
+import { ApiResponse } from "../types/DataAPI";
+
+export default async function Home() {
+    const response = await fetch('https://test-frontend-developer.s3.amazonaws.com/data/locations.json');
+    const { locations }: ApiResponse = await response.json();
+    
   return (
-    <main/>
+    <main>
+      {locations.map((local)=>{
+        return <p key={local.id}>{local.city_name}</p>;
+      })}
+    </main>
   );
 }
