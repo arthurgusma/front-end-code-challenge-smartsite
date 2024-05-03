0cf0b6d0d3d5ded9655c9f5f1b128b694a5d65c4
Arthur Gusmao <arthur.cabj@hotmail.com>
2024-05-03 11:12:25 -0300

    fix: removes unused import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
-import { ChakraProvider } from '@chakra-ui/react'
-import "./globals.css";
 import { Providers } from "./providers";
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Create Next App",
