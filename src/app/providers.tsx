4f3a3347c13f8cddaf69b2b3104a13afcf494523
Arthur Gusmao <arthur.cabj@hotmail.com>
2024-05-04 13:14:34 -0300

    refactor: default style for Radio component chraka

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,8 +2,27 @@
 
 import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 
+import { radioAnatomy } from '@chakra-ui/anatomy'
+import { createMultiStyleConfigHelpers } from '@chakra-ui/react'
+
+const { definePartsStyle, defineMultiStyleConfig } =
+  createMultiStyleConfigHelpers(radioAnatomy.keys)
+
+const baseStyle = definePartsStyle({
+  // define the part you're going to style
+  control: {
+    borderRadius: '24px', // change the border radius
+    borderColor: 'black', // change the border color
+  },
+})
+
+export const radioTheme = defineMultiStyleConfig({ baseStyle })
+
 export function Providers({ children }: { children: React.ReactNode }) {
   const theme = extendTheme({
+    components: {
+      Radio: radioTheme
+    },
     styles: {
       global: {
         "*": {
@@ -11,7 +30,6 @@ export function Providers({ children }: { children: React.ReactNode }) {
         }
       }
     },
-    
   });
   return <ChakraProvider theme={theme}>{children}</ChakraProvider>
 }
