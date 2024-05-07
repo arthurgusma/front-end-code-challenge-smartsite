0a52ae4f125d5e934a4d305c33bba6d1d52057cf
Arthur Gusmao <arthur.cabj@hotmail.com>
2024-05-06 21:59:19 -0300

    feat: form

---
+++ src/components/Form/RadioForm/index.tsx
@@ -0,0 +1,18 @@
+import { Flex, Radio, Text, Spacer} from '@chakra-ui/react'
+
+interface RadioFormProps {
+    value: string,
+    textLeft?: string,
+    textRight?: string,
+}
+
+export default function RadioForm({ value, textLeft, textRight}: RadioFormProps) {
+    return (
+        <Flex >
+            <Radio value={value} className='mr-2'/>
+            {textLeft && <Text fontSize='sm'>{textLeft}</Text>}
+            <Spacer />
+            {textRight && <Text fontSize='sm'>{textRight}</Text>}
+        </Flex>
+    );
+}
---
+++ src/components/Form/index.tsx
@@ -0,0 +1,98 @@
+'use client';
+import { FormEvent } from 'react';
+
+import iconHour from '../../../public/_material/images/icon-hour.png';
+
+import {
+    FormControl,
+    FormLabel,
+    FormHelperText,
+    RadioGroup,
+    VStack,
+    StackDivider,
+    Checkbox,
+    Divider, 
+    Flex, 
+    ButtonGroup,
+    Button
+  } from '@chakra-ui/react'
+
+import { useSmartFitContext } from '../../ContextAPI/SmartLocationsContext';
+
+import Image from 'next/image';
+import RadioForm from './RadioForm';
+
+export default function Form(){
+    const { 
+        getSmartFitLocations, 
+        setFilterRadioValue, 
+        setDisplayLocationsClosed, 
+        filterRadioValue, 
+        displayLocationsClosed, 
+        totalLocationsFound 
+    } = useSmartFitContext();
+
+    function handleSubmitSearch(ev: FormEvent) {
+        ev.preventDefault();
+        getSmartFitLocations();
+    }
+
+    return (
+        <section className='flex justify-center py-8'>
+          <div className='w-2/3 border-solid border-4 border-slate-100 p-4'>
+            <div className='flex items-center'>
+              <Image src={iconHour} alt="clock image" className='size-6 mr-2' />
+              <div className='text-sm opacity-60'>Horário</div>
+            </div>
+            <FormControl onSubmit={handleSubmitSearch}>
+              <FormLabel as='legend'>Qual período quer treinar?</FormLabel>
+              <RadioGroup onChange={setFilterRadioValue} value={filterRadioValue}>
+                <VStack 
+                  divider={<StackDivider borderColor='gray.200' />} 
+                  spacing={4} 
+                  align='stretch'
+                  className='opacity-60'
+                >
+                  <Divider />
+                  <RadioForm value='Manhã' textLeft='Manhã' textRight='06:00 às 12:00'/>
+                  <RadioForm value='Tarde' textLeft='Tarde' textRight='12:01 às 18:00'/>
+                  <RadioForm value='Noite' textLeft='Noite' textRight='18:01 às 23:00'/>
+                  <Divider />
+                </VStack>
+              </RadioGroup>
+              <FormHelperText className='flex justify-between'>
+                <div>
+                  <Checkbox className='mr-2' onChange={() => setDisplayLocationsClosed(!displayLocationsClosed)} />
+                  Exibir unidades fechadas
+                </div>
+                <div>Resultados encontrados: {totalLocationsFound}</div>
+              </FormHelperText>
+              <Flex className='justify-center p-4'>
+                <ButtonGroup gap='8' className='flex justify-center'>
+                  <Button 
+                    size='md'
+                    height='48px'
+                    width='360px'
+                    border='2px'
+                    colorScheme='yellow'
+                    borderColor='yellow.400'
+                    onClick={handleSubmitSearch}
+                  >
+                    ENCONTRAR UNIDADE
+                  </Button>
+                  <Button 
+                    height='48px'
+                    width='360px'
+                    border='2px'
+                    borderColor='gray.200'
+                    backgroundColor='inherit'
+                  >
+                    LIMPAR
+                  </Button>
+                </ButtonGroup>
+              </Flex>
+            </FormControl>
+          </div>
+        </section>
+      );
+}
