952f0eaf60acf769ff1201f2db7c930f5e411bef
Arthur Gusmao <arthur.cabj@hotmail.com>
2024-05-13 19:29:30 -0300

    fix: layout on smaller screen

--- src/components/DescriptionList/DescriptionItem/index.tsx
+++ src/components/DescriptionList/DescriptionItem/index.tsx
@@ -3,14 +3,16 @@ import { DescriptionItemProps } from "./types";
 
 export default function DescriptionItem({ title, itens }: DescriptionItemProps){
     return (
-        <div className="text-center">
+        <div className="text-center" >
          <h1 className="font-gotham-black mb-4">{title}</h1>
          <div className="xl:flex justify-center">
             {itens.map((item, index) => {
               return (
-                 <div key={index} className="m-auto mx-2">
-                     <Image src={item.image} alt={`${item.description} ilustration`} className="size-16" />
-                     <p>{item.description}</p>
+                 <div key={index} className="m-auto text-base text-xs sm:text-base">
+                    <div className="flex justify-center">
+                        <Image src={item.image} alt={`${item.description} ilustration`} className="size-16" />
+                    </div>
+                     <p >{item.description}</p>
                  </div>
                  )
             })}
--- src/components/DescriptionList/index.tsx
+++ src/components/DescriptionList/index.tsx
@@ -5,7 +5,7 @@ import { mask, towel, fountain, lockerroom } from './constDescriptions';
 export default function DescripitionList() {
     return (
         <section className='py-8'>
-            <div className='flex justify-center bg-slate-100 xl:space-x-4 md:space-x-8 sm:space-x-8 p-4'>
+            <div className='flex justify-center bg-slate-100 justify-between p-0 sm:p-4'>
                 <DescriptionItem title='Máscara' itens={mask} />
                 <DescriptionItem title='Toalha' itens={towel} />
                 <DescriptionItem title='Bebedouro' itens={fountain} />
